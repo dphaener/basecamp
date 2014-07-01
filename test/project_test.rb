7232693aa952f4c1faa132096561559b7031d01b
Darin Haener <dphaener@gmail.com>
2014-06-30 21:58:22 -0700

    Add Travis for build tests

---
+++ test/project_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+class ProjectTest < MiniTest::Test
+  context '#todolists' do
+    context 'when the request fails' do
+
+    end
+
+    context 'when the request is successful' do
+
+    end
+  end
+end
